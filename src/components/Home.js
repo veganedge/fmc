6d73f395c85f14d4da08166bdd54967230e84249
veganedge <scottxbrown@yahoo.com>
2022-05-16 21:18:12 -0700

    better comment of to-do list

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,20 +23,26 @@ export default Home;
 
 // === HOME ===
 
-// -- Navbar --
-// fix css for links
+// -- Background Image (Media queries) --
+// Mobile 480px wide
+// Tablet 768px wide by 1024px height
+
+// -- Size --
+// size properly so can't scroll vertically
+
+
+// === NAVBAR ===
+// make X icon for mobile when active
 
 // -- Darkmode --
 // Images in navbar after links
 // setup css classes for each element want changed
 // setup query to change classes?
 
-// -- Background Image (Media queries) --
-// Mobile 480px wide
-// Tablet 768px wide by 1024px height
-
 
 // === APP ===
+
+
 
 
 // === TEAM ===
@@ -45,15 +51,15 @@ export default Home;
 // Card:
 // team graphics that TJ made
 // and short description of role
-
 // media queries to make responsive layout for mobile, tablet, and desktop
 
+
+// === ABOUT ===
+
+// -- Layout --
+// show Discord, Team, and Whitepaper
 
 // === PUBLIC FOLDER ===
 
 // -- Need to put in favicon --
 // ask TJ for it
-
-
-// === CONTACT ===
-// gmail?  Necessary at all?
