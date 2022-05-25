ce4722f7941a2ea46276ed6514fbb2582c29d8ee
veganedge <scottxbrown@yahoo.com>
2022-05-25 05:37:35 -0700

    ItemCard component for Marketplace

--- src/components/dashboard/Marketplace.js
+++ src/components/dashboard/Marketplace.js
@@ -1,28 +1,23 @@
+import ItemCard from "./marketplace/ItemCard";
 
 
 const Marketplace = () => {
     return (
-        <div>
-            <div className='marketplace-images-container'>
-                <img src="https://via.placeholder.com/150?text=Item" alt="placeholder for marketplace item"></img>
-                <img src="https://via.placeholder.com/150?text=Item" alt="placeholder for marketplace item"></img>
-                <img src="https://via.placeholder.com/150?text=Item" alt="placeholder for marketplace item"></img>
-                <img src="https://via.placeholder.com/150?text=Item" alt="placeholder for marketplace item"></img>
-                <img src="https://via.placeholder.com/150?text=Item" alt="placeholder for marketplace item"></img>
-                <img src="https://via.placeholder.com/150?text=Item" alt="placeholder for marketplace item"></img>
-                <img src="https://via.placeholder.com/150?text=Item" alt="placeholder for marketplace item"></img>
-                <img src="https://via.placeholder.com/150?text=Item" alt="placeholder for marketplace item"></img>
-                <img src="https://via.placeholder.com/150?text=Item" alt="placeholder for marketplace item"></img>
-                <img src="https://via.placeholder.com/150?text=Item" alt="placeholder for marketplace item"></img>
-            </div>  
-            <div className='button-container'>
-                <button className='chart-button'>Button 1</button>
-                <button className='chart-button'>Button 2</button>
-                <button className='chart-button'>Button 3</button>
-                <button className='chart-button'>Button 4</button>
-                <button className='chart-button'>Button 5</button>
-            </div>
-       </div>
+        <div className='marketplace-images-container'>
+            <ItemCard />
+            <ItemCard />
+            <ItemCard />
+            <ItemCard />
+            <ItemCard />
+            <ItemCard />
+            <ItemCard />
+            <ItemCard />
+            <ItemCard />
+            <ItemCard />
+            <ItemCard />
+            <ItemCard />
+        </div>  
+        
     )
 }
 
