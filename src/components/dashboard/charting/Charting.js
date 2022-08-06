ad64fcd6bd4711f6f63fc4abfdc82a76369d6d7b
veganedge <scottxbrown@yahoo.com>
2022-08-05 17:16:29 -0700

    updated CHARTING.js

--- src/components/dashboard/charting/Charting.js
+++ src/components/dashboard/charting/Charting.js
@@ -9,9 +9,9 @@ const Charting = () => {
             </div>
             <div className={classes.buttonContainer}>
                 <button>Button 1</button>
-                <button>Button 4</button>
                 <button>Button 2</button>
                 <button>Button 3</button>
+                <button>Button 4</button>
                 <button>Button 5</button>
             </div>
         </div>
